cf904f48627c6edc82f7bd2698f108f56ed4144b
theMorshed <morshedfive@gmail.com>
2022-08-11 11:12:57 +0600

    you can sort certain amount of item using vector sort function

---
+++ vector_range_sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+// function for reverse sort
+bool vector_reverse(int a, int b)
+{
+    return a > b;
+}
+
+int main()
+{
+    // declare a vector and iterator
+    vector <int> numbers;
+    vector <int>::iterator num;
+
+    // add some data using push_back function
+    numbers.push_back(10);
+    numbers.push_back(432);
+    numbers.push_back(32);
+    numbers.push_back(52);
+    numbers.push_back(12);
+    numbers.push_back(50);
+
+    // sort vector using sort function 
+    sort(numbers.begin(), numbers.begin() + 3);
+    sort(numbers.begin(), numbers.begin() + 3, vector_reverse);
+
+    // print the whole vector using iterator
+    cout << "All sorted numbers: ";
+    for(num = numbers.begin(); num != numbers.end(); num++)
+    {
+        cout << *num << "   ";
+    }
+    cout << endl;
+
+    return 0;
+}
