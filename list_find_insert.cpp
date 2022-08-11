7cbb6ed80013ec5d3e20cce011c852050dbd15e8
theMorshed <morshedfive@gmail.com>
2022-08-11 16:28:09 +0600

    update find insert file of list

--- list_find_insert.cpp
+++ list_find_insert.cpp
@@ -14,11 +14,25 @@ int main()
     list <int> mylist(arr, arr + 5);
     list <int>::iterator list_item;
 
-    // find an specific item index
+    // find an specific item index if item not present in the list iterator return end     
     list_item = find(mylist.begin(), mylist.end(), 1);
+
+    // check item is present
+    if (list_item == mylist.end())
+    {
+        cout << "NOT FOUND\n";        
+    }
+    else
+    {
+        cout << *list_item << endl;
+    }
+
     // print the find result
-    cout << *list_item << endl;
     mylist.insert(list_item, 9);
+
+    // erase an item from list
+    list_item = find(mylist.begin(), mylist.end(), 2);
+    mylist.erase(list_item);
 
     // print whole list
     for (list_item = mylist.begin(); list_item != mylist.end(); list_item++)
